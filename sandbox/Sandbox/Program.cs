e9fdfdb617c6a1d7233146d5abfe888ed5b511bf
TheSergei <squintanilla@byupathway.edu>
2025-12-30 08:40:02 -0600

    week one exercise 1,2,3 and part of 4

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Hello C# can be very hard but I am willing to try again.");
+        Console.WriteLine("How many times have you tried learning C#?");
+        Console.ReadLine();
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userInput = int.Parse(Console.ReadLine());
+
+        List<int> numbersList = new List<int>();
+
+        while (userInput > 0)
+        {       
+            Console.WriteLine("Enter a number:");
+            int addedNumber = int.Parse(Console.ReadLine());
+
+            numbersList.Add(userInput);
+
+            int listSum = numbersList.Sum();
+
+        if (addedNumber == 0)
+        {
+            Console.WriteLine($"the sum is {listSum}");
+        }
+        }
+            
+
+
+        
     }
 }

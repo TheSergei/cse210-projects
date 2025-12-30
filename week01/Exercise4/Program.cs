0fc1e43d9e2bf690909dd77e371096b199db9368
TheSergei <squintanilla@byupathway.edu>
2025-12-30 10:56:59 -0600

    fininshed exercise 4 learning about LINQ and im very proud of it

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -2,33 +2,40 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-        int userInput = int.Parse(Console.ReadLine());
+        List<int> numbers = new List<int>();        //make a new list 
 
-        List<int> numbersList = new List<int>();
+        int userNumber = -1;        //make sure this is less than 0 or else the program wont prompt user
 
-        while (userInput > 0)
-        {       
-            Console.WriteLine("Enter a number:");
-            int addedNumber = int.Parse(Console.ReadLine());
-
-            numbersList.Add(userInput);
-
-            int listSum = numbersList.Sum();
-
-        if (addedNumber == 0)
+        while (userNumber != 0)     // generate while loop carefully
         {
-            Console.WriteLine($"the sum is {listSum}");
-        }
-        }
+            Console.Write("Enter a number, (0 to quit): ");
             
+            string userResponse = Console.ReadLine();       // this is the key to not have an infinite loop!!!!!!
+            userNumber = int.Parse(userResponse);
 
+            if (userNumber > 0)
+            {
+                numbers.Add(userNumber);        //add input into list
+            }
+        }
+
+        int listSum = numbers.Sum();
+        Console.WriteLine($"the sum is {listSum}");     //compute sum of all numbers using LINQ
 
         
+        
+        double avg = numbers.Average();     //compute avg using LINQ
+        Console.WriteLine($"the avg is {avg}");
+        
+
+        int max = numbers.Max();        //compute max or largest number in the list using LINQ
+        Console.WriteLine($"the max is {max}");
     }
+
 }
